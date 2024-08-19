c9674b6c539408adc53ad5185e051bf32db63098
pcpetty <99506374+pcpetty@users.noreply.github.com>
2024-08-19 10:37:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -512,7 +512,7 @@ with st.container():
 
     # HTML form with POST method
     contact_form = """
-    <form action="https://formsubmit.co/colepetty57@gmail.com" method="POST">
+    <form action="https://formsubmit.co/colepetty57@gmail.com" method="POST" />
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
