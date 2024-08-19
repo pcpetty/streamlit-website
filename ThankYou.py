1b00b419032493fa70a2ccc9a0d903f8945cd265
pcpetty <99506374+pcpetty@users.noreply.github.com>
2024-08-19 10:51:42 -0400

    Update and rename app (2).py to ThankYou.py

---
+++ ThankYou.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+# Thank You Page
+def thank_you():
+    st.write("---")
+    st.header("Thank You!")
+    st.write("##")
+    st.write("Your message has been successfully submitted. I will get back to you soon.")
