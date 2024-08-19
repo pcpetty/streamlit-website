9759444b524422314031dff98ab76357d66e963e
pcpetty <99506374+pcpetty@users.noreply.github.com>
2024-08-19 09:40:36 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,10 +1,10 @@
 
 import streamlit as st
 
-def app():
-    st.title("Welcome to My Data Scientist Portfolio")
-    st.write("This is the homepage of the portfolio.")
-    # Add your homepage content here
+# def app():
+#     st.title("Welcome to My Data Scientist Portfolio")
+#     st.write("This is the homepage of the portfolio.")
+#     # Add your homepage content here
 
-if __name__ == '__main__':
-    app()
+# if __name__ == '__main__':
+#     app()
