e7aeb5869b2d4163ff63758ba5b16b53cca0ced1
pcpetty <99506374+pcpetty@users.noreply.github.com>
2024-08-19 09:05:21 -0400

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,10 @@
+
+import streamlit as st
+
+def app():
+    st.title("Welcome to My Data Scientist Portfolio")
+    st.write("This is the homepage of the portfolio.")
+    # Add your homepage content here
+
+if __name__ == '__main__':
+    app()
